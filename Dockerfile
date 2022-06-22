ab01c40f18d710390d35ddfbbc7818404b4cfb32
ryanbsdeveloper <ryanbsdeveloper@gmail.com>
2022-06-22 08:28:09 -0300

    update for heroku

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-
+CMD [ "uvicorn", "src.main:app" ]
--- src/controllers/database.py
+++ src/controllers/database.py
@@ -3,7 +3,7 @@ import sqlalchemy.ext.declarative as declarative
 import sqlalchemy.orm as orm
 
 
-DATABASE_URL = f"postgresql://ryanl:password@db:5432/fastapi_database"
+DATABASE_URL = f"postgres://wtrsiuspjnorvg:707f69c0ec822ee5e4e5a2a84151d5adaee9672af69e357aac33cfe54cead93b@ec2-23-23-182-238.compute-1.amazonaws.com:5432/d9jr2mvsnnnsd1"
 
 engine = sql.create_engine(DATABASE_URL)
 
